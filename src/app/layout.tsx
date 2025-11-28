1ea112e76d7bc2e98e5e7ff749061b4b0f4bff5a
shubham9049 <shubham.rajveer19@gmail.com>
2025-11-28 16:14:17 +0530

    Add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,20 @@ export default function RootLayout({
         name="google-site-verification"
         content="yoASm41IVFHOXhRAFs956RnT4fR_WYzHiZBbvhR6ILU"
       />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6LCRCK2W3C"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-6LCRCK2W3C');
+  `}
+      </Script>
+
       <style>{`
           body > .skiptranslate {
             display: none;
